4fa0ef764c02a563ef05e5c61f865138b21ec701
UmmeRija <ummerijababar@gmail.com>
2025-04-04 20:23:37 +0500

    third task done

--- third.dart
+++ third.dart
@@ -3,9 +3,10 @@ void main(){
 // and write a program that prints out all the elements of the list that are less than 5.
 
 List <int> number = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-int result = number.;
-if(number==())
-number.forEach((a){
-});
-
+for (var data in number) {
+    if(data<=5){
+        print(data);
+    }
+  
+}
 }
